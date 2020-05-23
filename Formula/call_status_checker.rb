da773a760a1cdd76a8925d751eebd137b1490835
Andrew Seward <awseward@users.noreply.github.com>
2020-05-22 19:07:54 -0700

    call_status_checker 0.3.5
    Sourced from https://github.com/awseward/call_status/releases/tag/0.3.5.

--- Formula/call_status_checker.rb
+++ Formula/call_status_checker.rb
@@ -1,8 +1,8 @@
 class CallStatusChecker < Formula
   desc     "Checks Zoom to update call status"
   homepage "https://github.com/awseward/call_status"
-  url "https://github.com/awseward/call_status/releases/download/0.3.4/call_status_checker-0.3.4.tar.gz"
-  sha256 "64590cc7ae8fd26ae7c47d3ad1940b1b67169c3a1c971a581ebf1192632793d7"
+  url "https://github.com/awseward/call_status/releases/download/0.3.5/call_status_checker-0.3.5.tar.gz"
+  sha256 "ec4298ea1ce8948ec6d5a2bfb802454da199bf04f8c4030c585d931ba3ec2223"
 
   bottle :unneeded
 
