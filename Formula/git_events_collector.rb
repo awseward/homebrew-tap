7bbb99e434142da05b381a05ee620e17480e7ee4
Andrew Seward <awswrd@gmail.com>
2020-12-08 20:57:59 -0800

    Add a very basic formula

---
+++ Formula/git_events_collector.rb
@@ -0,0 +1,40 @@
+class CallStatusChecker < Formula
+  desc     "TODO"
+  homepage "https://github.com/awseward/git_events_collector"
+  url "https://github.com/awseward/git-events-collector/releases/download/0.0.1/git_events_collector-0.0.1.tar.gz"
+  sha256 "485d5433e1c9a6781275b6a595227618739a5a0597a72f17e2193beebf402fec"
+
+  bottle :unneeded
+
+  def install
+    all_bin.each { |f| bin.install f }
+  end
+
+  test do
+    bin_full_path = "/usr/local/bin/#{nim_bin}"
+
+    system bin_full_path, "--version"
+    system bin_full_path, "--revision"
+  end
+
+  private
+
+  def all_bin
+    [
+      nim_bin,
+      *shell_bin
+    ]
+  end
+
+  def nim_bin
+    "git_events_collector"
+  end
+
+  def shell_bin
+    %w[
+      rotate.sh
+      to_sqlite.sh
+      push.sh
+    ]
+  end
+end
