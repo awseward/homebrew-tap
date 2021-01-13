4628997735fe24a9976c93e4bee77bfc34c7ebfe
Andrew Seward <awseward@users.noreply.github.com>
2021-01-12 21:10:12 -0800

    uq 0.1.7
    Sourced from https://github.com/awseward/uq/releases/tag/0.1.7.

--- Formula/uq.rb
+++ Formula/uq.rb
@@ -1,8 +1,8 @@
 class Uq < Formula
   desc 'Like `jq`, but for URLs.'
   homepage 'https://github.com/awseward/uq'
-  url 'https://github.com/awseward/uq/releases/download/0.1.6/uq-0.1.6-macos.tar.gz'
-  sha256 'e527b40f6c9a52bdd234ad21bd27961dd85105b08a56c3dba8d0281132938d36'
+  url 'https://github.com/awseward/uq/releases/download/0.1.7/uq-0.1.7-macos.tar.gz'
+  sha256 'a4843a0e1e9d4e58dca26889d13efce9bb55aac5db0f9a756575330b7d882fff'
 
   bottle :unneeded
 
